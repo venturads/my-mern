426176e82a85a715c3333103c137ca7d43c6b3a3
Albert Ventura <albert@venturads.com>
2018-02-19 19:06:49 +0000

    init git

---
+++ merncommentbox/src/index.js
@@ -0,0 +1,8 @@
+//index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './CommentBox';
+ReactDOM.render(
+ <CommentBox />,
+ document.getElementById('root')
+);
