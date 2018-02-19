7c18b7915780d5813f49452a49e73776f0904a9b
Albert Ventura <albert@venturads.com>
2018-02-19 20:50:21 +0000

    crud mern app

--- merncommentbox/src/index.js
+++ merncommentbox/src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 ReactDOM.render(
- <CommentBox />,
+ <CommentBox
+ url='https://my-mern-venturads.c9users.io/3000/api/comments' 
+ pollInterval={2000} />,
  document.getElementById('root')
 );
